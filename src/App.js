d087184ae568d6c93abee16e02767967694b479f
VitoNez5198 <113071024+VitoNez5198@users.noreply.github.com>
2023-01-31 22:24:19 -0300

    Eliminacion de archivos y aplicacion de bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (<>
+    <Container >
+      <Row className='bg-white' >
+        <Col>
+          <h1 className='p-5 m-auto bg-dark text-white d-flex justify-content-center'>Lista de Pokémon</h1>
+          <p>Pokémon es una franquicia de videojuegos que comenzó siendo de tipo RPG para las consolas de Nintendo creada por Satoshi Tajiri en 1995. Esta es la lista de Pokémon completa ordenada según el número en la Pokédex Nacional. En esta se muestra el nombre del Pokémon en español.</p>
+          <Button variant="primary">Learn More</Button>
+        </Col>
+      </Row>
+    </Container>
+    <footer className='bg-dark text-white p-5 justify-content-center '>      
+    Made with love by&nbsp;<a href='https://github.com/VitoNez5198'> VitoNez </a>.&nbsp;This website is not produced, endorsed, supported, or affiliated with Nintendo or The Pokémon Company. Pokémon and Pokémon character names are trademarks of Nintendo.</footer>
+  </>
   );
 }
 
 export default App;
+
