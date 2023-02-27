1ef31331dc7223a4a78d27cefea43ff7b2d4ea73
VitoNez5198 <113071024+VitoNez5198@users.noreply.github.com>
2023-02-27 20:16:17 -0300

    Cambio icono footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
             <Row>
                 <Col>
                     <p className='bg-dark text-white mt-2 justify-content-center text-center'>
-                        Made with love by&nbsp;<a href='https://github.com/VitoNez5198'> VitoNez </a>.&nbsp;This website is not produced, endorsed, supported, or affiliated with Nintendo or The Pokémon Company. Pokémon and Pokémon character names are trademarks of Nintendo.</p>
+                        Made with ❤ by&nbsp;<a href='https://github.com/VitoNez5198'> VitoNez </a>.&nbsp;This website is not produced, endorsed, supported, or affiliated with Nintendo or The Pokémon Company. Pokémon and Pokémon character names are trademarks of Nintendo.</p>
                 </Col>
             </Row>
         </Container>
